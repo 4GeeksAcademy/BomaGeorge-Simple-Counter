77c45715d3671eae0c1d8bec767cffde4c1afba8
Boma G <51271912+BOMAURIEL@users.noreply.github.com>
2025-09-17 19:12:28 +0000

    fix error with bootstrap

--- src/js/components/SecondsCounter.jsx
+++ src/js/components/SecondsCounter.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 
 function SecondsCounter(props) {
-  // Always show 6 digits, padding with zeros
+  // Convert seconds into a 6-digit string (e.g., "000006")
   const secondsString = props.seconds.toString().padStart(6, "0");
   const digits = secondsString.split("");
 
   return (
     <div className="d-flex justify-content-center bg-black text-white p-3 fs-1">
+      {/* Solid FontAwesome clock icon */}
       <div className="digit mx-1">
         <i className="fa-solid fa-clock"></i>
       </div>
+
+      {/* Render each digit in its own box */}
       {digits.map((digit, index) => (
         <div key={index} className="digit mx-1">
           {digit}
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,21 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-
-import "bootstrap"
-
-import "@fortawesome/fontawesome-free/css/all.min.css";
-
-
-import '../styles/index.css'
-
-// components
+import React from "react";
+import ReactDOM from "react-dom/client";
 import Home from "./components/Home.jsx";
 
+// import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap"
+import "../styles/index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
-)
-
-
+    <Home />
+  </React.StrictMode>
+);
